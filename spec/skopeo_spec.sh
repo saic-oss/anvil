eabb55d2063a2d13d9019ee3f54547a11123be02
stascavageb <brian.stascavage@saic.com>
2021-08-13 13:50:38 -0400

    Adding logic to deal with skopeo having different versions

--- spec/skopeo_spec.sh
+++ spec/skopeo_spec.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env shellspec
 
 Describe "printVersion()"
-  printVersion() {
-    skopeo -v | grep "${SKOPEO_VERSION}"
-  }
+printVersion() {
+  skopeo -v | grep "${SKOPEO_BINARY_VERSION}"
+}
 
-  It "validates tool is installed by checking version"
-    When call printVersion
-    The output should include "${SKOPEO_VERSION}"
-    The status should eq 0
-  End
+It "validates tool is installed by checking version"
+When call printVersion
+The output should include "${SKOPEO_BINARY_VERSION}"
+The status should eq 0
+End
 End
