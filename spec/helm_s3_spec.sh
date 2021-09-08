46118352c82c5ce4b73be34e095a743b8d9bd33c
Sam Caneer <slcaneer42@students.tntech.edu>
2021-09-08 15:19:26 -0500

    add s3 shellspec test

---
+++ spec/helm_s3_spec.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env shellspec
+
+Describe "printVersion()"
+  printVersion() {
+    helm plugin list | grep "helm-s3" | grep "${HELM_S3_VERSION}"
+  }
+
+  It "validates tool is installed by checking version"
+    When call printVersion
+    The output should include "${HELM_S3_VERSION}"
+    The status should eq 0
+  End
+End
